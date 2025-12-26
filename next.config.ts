1c6940d54ece673ac308121396d0302d07cc4b89
Bryan Cannoy <notbryancannoy@gmail.com>
2025-12-25 20:33:37 -0500

    ad ga to csp

--- next.config.ts
+++ next.config.ts
@@ -44,11 +44,11 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com",
               "style-src 'self' 'unsafe-inline'",
               "img-src 'self' data: https:",
               "font-src 'self' data:",
-              "connect-src 'self' https://en.wikipedia.org",
+              "connect-src 'self' https://en.wikipedia.org https://www.google-analytics.com https://analytics.google.com",
               "frame-ancestors 'none'",
               "base-uri 'self'",
               "form-action 'self'",
